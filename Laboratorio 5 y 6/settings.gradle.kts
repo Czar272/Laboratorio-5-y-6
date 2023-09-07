0341949ec17aef2640a8292ec92c039523f5875d
Czar272 <110710742+Czar272@users.noreply.github.com>
2023-09-07 08:00:28 -0600

    Add files via upload

---
+++ Laboratorio 5 y 6/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Laboratorio5y6"
+include(":app")
+ 
